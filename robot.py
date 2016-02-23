efc9a583ab4db9b63b3af613e0a5cf0c0d95285c
psc_aw1 <psc_aw1@srobo.org>
2016-02-23 13:23:45 +0000

    bugfix

--- robot.py
+++ robot.py
@@ -37,22 +37,20 @@ S = motorHandler.motorHandler(R.motors[0].m0, R.motors[0].m1) #left motor, right
 print "motorHandler setup"
 
 while True:
-   
-   if (S.steering < 5 & S.steering > -5):    
+    if (S.steering < 5 & S.steering > -5):
         currentTime = time.time()
         markers = R.see( res=(1280,960) )         ## Takes a picture and analyses it at a resolution. For information on which resolutions can vbe used: https://www.studentrobotics.org/docs/programming/sr/vision/#ChoosingResolution
-        
         print "I can see", len(markers), "markers:"       ## Prints out how many markers by taking the length of the markers array
         print " "       ##line clear
-        
+         
         M.update(markers, currentTime)
-        
+         
         print "Cameralocation: ", M.cameraLocation
         print "aCubeLocations: ", M.aCubeLocations
         print "bCubeLocations: ", M.bCubeLocations
         print "cCubeLocations: ", M.cCubeLocations
         print "robotLocations: ", M.robotLocations
-    
+        
     if (D.updateAll() == True):
         
         if (P.run(D.yaw) == True):
@@ -60,16 +58,7 @@ while True:
             
             if (S.setSpeedAndSteering(speed, steering) == True):
                 print "yaw: ", D.yaw, " pitch:", D.pitch, " roll: ", D.roll, " error: ", D.error, " speed: ", S.speed, " steering: ", S.steering
-        
-    
-    
-        
-        
-        
-        
-        
-        
-        
+
         
         
     
