515f74e0041a2512418b07f17fe6f3bac7a66c41
psc_aw1 <psc_aw1@srobo.org>
2016-02-23 13:21:09 +0000

    Commit message

--- robot.py
+++ robot.py
@@ -37,20 +37,21 @@ S = motorHandler.motorHandler(R.motors[0].m0, R.motors[0].m1) #left motor, right
 print "motorHandler setup"
 
 while True:
-    
-    currentTime = time.time()
-    markers = R.see( res=(1280,960) )         ## Takes a picture and analyses it at a resolution. For information on which resolutions can vbe used: https://www.studentrobotics.org/docs/programming/sr/vision/#ChoosingResolution
-    
-    print "I can see", len(markers), "markers:"       ## Prints out how many markers by taking the length of the markers array
-    print " "       ##line clear
-    
-    M.update(markers, currentTime)
-    
-    print "Cameralocation: ", M.cameraLocation
-    print "aCubeLocations: ", M.aCubeLocations
-    print "bCubeLocations: ", M.bCubeLocations
-    print "cCubeLocations: ", M.cCubeLocations
-    print "robotLocations: ", M.robotLocations
+   
+   if (S.steering < 5 & S.steering > -5):    
+        currentTime = time.time()
+        markers = R.see( res=(1280,960) )         ## Takes a picture and analyses it at a resolution. For information on which resolutions can vbe used: https://www.studentrobotics.org/docs/programming/sr/vision/#ChoosingResolution
+        
+        print "I can see", len(markers), "markers:"       ## Prints out how many markers by taking the length of the markers array
+        print " "       ##line clear
+        
+        M.update(markers, currentTime)
+        
+        print "Cameralocation: ", M.cameraLocation
+        print "aCubeLocations: ", M.aCubeLocations
+        print "bCubeLocations: ", M.bCubeLocations
+        print "cCubeLocations: ", M.cCubeLocations
+        print "robotLocations: ", M.robotLocations
     
     if (D.updateAll() == True):
         
