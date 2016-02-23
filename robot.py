ceec52227205a24e8c0da4195172281e6f39c956
psc_aw1 <psc_aw1@srobo.org>
2016-02-23 12:02:38 +0000

    classes set up

---
+++ robot.py
@@ -0,0 +1,55 @@
+from sr.robot import *
+import time
+import customRuggeduino
+import PID
+import motorHandler
+  
+print "Classes imported"
+
+R = Robot.setup()
+print "Robot.setup ran"
+
+# Register the custom class with the Robot object
+R.ruggeduino_set_handler_by_fwver("SRcustom", customRuggeduino.mpuRuggeduino)
+print "Ruggeduino handler ran"
+
+R.init()
+print "R.init ran"
+
+M = customRuggeduino.mpuHandler(R.ruggeduinos[0])
+print "mpuHandler initialised"
+
+R.wait_start()
+print "wait_start ran"
+
+targetYaw = 0
+speed = 30
+
+P = PID.pidController("steeringPID", 1.5, 2, 0, targetYaw) #p, i, d, setpoint
+print "PID setup"
+
+S = motorHandler.motorHandler(R.motors[0].m0, R.motors[0].m1) #left motor, right motor
+print "motorHandler setup"
+
+while True:
+    
+    if (M.updateAll() == True):
+        
+        if (P.run(M.yaw) == True):
+            steering = P.output
+            
+            if (S.setSpeedAndSteering(speed, steering) == True):
+                print "yaw: ", M.yaw, " pitch:", M.pitch, " roll: ", M.roll, " error: ", M.error, " speed: ", S.speed, " steering: ", S.steering
+        
+    
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
