a453a41f3f6d9fafa4d69f881310adff0527a472
psc_aw1 <psc_aw1@srobo.org>
2016-02-28 19:40:15 +0000

    bug fix

--- robot.py
+++ robot.py
@@ -1,3 +1,12 @@
+yawDrift = -0.004 #degrees per second 
+targetYaw = 0
+speed = 30
+
+kp = 3
+ki = 2
+kd = 3
+iLimit = 100
+
 from sr.robot import *
 import time
 import customRuggeduino
@@ -17,20 +26,17 @@ print "Ruggeduino handler ran"
 R.init()
 print "R.init ran"
 
-D = customRuggeduino.mpuHandler(R.ruggeduinos[0])
+D = customRuggeduino.mpuHandler(R.ruggeduinos[0], yawDrift)
 print "mpuHandler initialised"
 
 R.wait_start()
 print "wait_start ran"
 
-targetYaw = 0
-speed = 30
-
 currentTime = time.time()
 zone = R.zone
 M = mapHandler.mapHandler(zone, currentTime)
 
-P = PID.pidController("steeringPID", 3, 2, 3, targetYaw, 100) #p, i, d, setpoint
+P = PID.pidController("steeringPID", kp, ki, kd, targetYaw, iLimit) #p, i, d, setpoint, iLimit, startingI
 print "PID setup"
 
 S = motorHandler.motorHandler(R.motors[0].m0, R.motors[0].m1) #left motor, right motor
@@ -40,11 +46,11 @@ while True:
     
     if (D.updateAll() == True):
         
-        if (P.run(D.yaw) == True):
+        if (P.run(D.yawWithoutDrift) == True):
             steering = P.output
             
             if (S.setSpeedAndSteering(speed, steering) == True):
-                print "yaw: ", D.yaw, " pitch:", D.pitch, " roll: ", D.roll, " error: ", D.error, " speed: ", S.speed, " steering: ", S.steering
+                print "yaw: ", D.yaw, " yawWithoutDrift: ", D.yawWithoutDrift, " pitch:", D.pitch, " roll: ", D.roll, " error: ", D.error, " speed: ", S.speed, " steering: ", S.steering
 
     if ((S.steering < 2) and (S.steering > -2)):
         currentTime = time.time()
