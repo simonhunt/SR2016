c9208dc2a6df3a256d1f2dc1ca79781a65eecebb
psc_aw1 <psc_aw1@srobo.org>
2016-02-28 21:51:16 +0000

    added angleMod function

--- mapToLimits.py
+++ mapToLimits.py
@@ -1,8 +1,22 @@
 def mapToLimits(value, upperLimit = 100, lowerLimit = - 100):
-        output = value
-        if (value > upperLimit):
-            output = upperLimit
-            
-        elif (value < lowerLimit):
-            output = lowerLimit
-        return output
+    output = value
+    
+    if (value > upperLimit):
+        output = upperLimit
+        
+    elif (value < lowerLimit):
+        output = lowerLimit
+    return output
+    
+def angleMod(angle):
+    output = angle
+    
+    while (output > 180):
+        output -= 360
+        
+    while (output <= -180):
+        output += 360
+        
+    return output
+    
+    
