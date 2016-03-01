7ab959f179f4020e9dbf83ba86beff37361d47e5
psc_aw1 <psc_aw1@srobo.org>
2016-03-01 21:39:16 +0000

    currentTime => current_time

--- ruggeduino.py
+++ ruggeduino.py
@@ -49,8 +49,8 @@ class MpuSonarRuggeduino(Ruggeduino):
 class MpuHandler():
     
     def __init__(self, MpuRuggeduino, yaw_drift, time_period = 0.025, mpu_start_timeout = 2): # default 40Hz
-        currentTime = time.time()
-        self.innit_time = currentTime
+        current_time = time.time()
+        self.innit_time = current_time
         self.time_period = time_period
         self.MpuRuggeduino = MpuRuggeduino
         self.MpuRuggeduino.mpuInit()
@@ -60,20 +60,20 @@ class MpuHandler():
         self.pitch = 0
         self.roll = 0
         self.error = 0
-        self.last_yaw_time = currentTime + mpu_start_timeout - time_period
-        self.last_pitch_time = currentTime + mpu_start_timeout - time_period
-        self.last_roll_time = currentTime + mpu_start_timeout - time_period
-        self.last_error_time = currentTime + mpu_start_timeout - time_period
+        self.last_yaw_time = current_time + mpu_start_timeout - time_period
+        self.last_pitch_time = current_time + mpu_start_timeout - time_period
+        self.last_roll_time = current_time + mpu_start_timeout - time_period
+        self.last_error_time = current_time + mpu_start_timeout - time_period
         
     def setTimePeriod(self, time_period):
         self.time_period = time_period
         
     def updateAll(self):
-        currentTime = time.time()
-        dyt = currentTime - self.last_yaw_time
-        dpt = currentTime - self.last_pitch_time
-        drt = currentTime - self.last_roll_time
-        det = currentTime - self.last_error_time
+        current_time = time.time()
+        dyt = current_time - self.last_yaw_time
+        dpt = current_time - self.last_pitch_time
+        drt = current_time - self.last_roll_time
+        det = current_time - self.last_error_time
         updated = False
         
         if ((dyt >= self.time_period) and (dpt >= self.time_period) and (drt >= self.time_period) and (det >= self.time_period)):
@@ -81,10 +81,10 @@ class MpuHandler():
             self.pitch = self.MpuRuggeduino.mpuGetPitch()
             self.roll = self.MpuRuggeduino.mpuGetRoll()
             self.error = self.MpuRuggeduino.mpuGetError()
-            self.last_yaw_time = currentTime
-            self.last_pitch_time = currentTime
-            self.last_roll_time = currentTime
-            self.last_error_time = currentTime
+            self.last_yaw_time = current_time
+            self.last_pitch_time = current_time
+            self.last_roll_time = current_time
+            self.last_error_time = current_time
             updated = True 
         self.updateYawWithoutDrift()
         return updated
@@ -95,45 +95,45 @@ class MpuHandler():
         self.yaw_without_drift = angleMod(self.yaw - drift)
                 
     def updateYaw(self):
-        currentTime = time.time()
-        dyt = currentTime - self.last_yaw_time
+        current_time = time.time()
+        dyt = current_time - self.last_yaw_time
         updated = False
         
         if (dyt >= self.time_period):
             self.yaw = self.MpuRuggeduino.mpuGetYaw()
-            self.last_yaw_time = currentTime
+            self.last_yaw_time = current_time
             updated = True
         return updated
         
     def updatePitch(self):
-        currentTime = time.time()
-        dpt = currentTime - self.last_pitch_time
+        current_time = time.time()
+        dpt = current_time - self.last_pitch_time
         updated = False
         
         if (dpt >= self.time_period):
             self.pitch = self.MpuRuggeduino.mpuGetPitch()
-            self.last_pitch_time = currentTime
+            self.last_pitch_time = current_time
             updated = True
         return updated
         
     def updateRoll(self):
-        currentTime = time.time()
-        drt = currentTime - self.last_roll_time
+        current_time = time.time()
+        drt = current_time - self.last_roll_time
         updated = False
         
         if (drt >= self.time_period):
             self.roll = self.MpuRuggeduino.mpuGetRoll()
-            self.last_roll_time = currentTime            
+            self.last_roll_time = current_time            
             updated = True
         return updated
         
     def updateError(self):
-        currentTime = time.time()
-        det = currentTime - self.last_error_time
+        current_time = time.time()
+        det = current_time - self.last_error_time
         updated = False
         
         if (det >= self.time_period):
             self.error = self.MpuRuggeduino.mpuGetError()
-            self.last_error_time = currentTime
+            self.last_error_time = current_time
             updated = True
         return updated
