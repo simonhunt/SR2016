c04f83ed9cc095182ca5e7b6470c2e7db2c6adf3
psc_aw1 <psc_aw1@srobo.org>
2016-03-03 22:53:50 +0000

    t

--- multi.py
+++ multi.py
@@ -1,1 +1,30 @@
-pass
+import threading
+from limits import angleMod
+
+heading = [True, 0]
+
+def changeHeading(angle):
+    global heading
+    heading[0] = True
+    heading[0] = angleMod(
+
+class YawThread(threading.Thread):
+    
+    def __init__(self, YawPid, MpuHandler):
+        threading.Thread.__init__(self)
+        self.Y = YawPid
+        self.D = MpuHandler
+    
+    def run(self):
+        print "Starting YawThread"
+        
+        if (self.D.updateAll() == True):
+            pass
+            
+        print "Exiting " + self.name
+
+
+    
+    
+        
+        
