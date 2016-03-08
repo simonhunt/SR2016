e5be9019d8ed2c95deb3f199f6ba6585329d085b
psc_aw1 <psc_aw1@srobo.org>
2016-03-08 11:51:22 +0000

    added motor thread

--- multi.py
+++ multi.py
@@ -15,13 +15,14 @@ class YawThread(threading.Thread):
     
     def __init__(self, YawPid, MpuHandler):
         threading.Thread.__init__(self)
+        self.name = "YawThread"
         self.steering = 0
         self.steering_updated = True
         self.Y = YawPid
         self.D = MpuHandler
     
     def run(self):
-        print "Starting YawThread"
+        print "Starting" + self.name
         
         while (True):
             
@@ -35,7 +36,24 @@ class YawThread(threading.Thread):
                 self.steering_updated = True
                 
         print "Exiting " + self.name
-
+        
+class MotorThread(threading.Thread):
+    
+    def __init__(self, MotorHandler):
+        threading.Thread.__init__(self)
+        self.name = "MotorThread"
+        self.steering = 0
+        self.speed = 0
+        self.S = MotorHandler
+    
+    def run(self):
+        print "Starting" + self.name
+        while (True):
+            
+           self.S.setSpeedAndSteering(self.speed, self.steering)
+                
+        print "Exiting " + self.name
+        
 
     
     
