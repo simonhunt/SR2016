36fcfb0e63c82b2f3211d08e4ed1b6283df4a72e
psc_aw1 <psc_aw1@srobo.org>
2016-03-15 22:32:54 +0000

    needs more work

--- multi.py
+++ multi.py
@@ -16,9 +16,6 @@ class MotionThread(threading.Thread):
         self.speed = 0
         self.yaw = 0
         self.desired_yaw = 0
-        self.forced_speed = 0
-        self.forced_speed_available = True
-        self.new_forced_speed = False
         self.displacement = 0
         self.desired_displacement = 0
         self.D = MpuHandler
@@ -31,15 +28,6 @@ class MotionThread(threading.Thread):
         self.action = STILL
         self.action_value = 0
         self.action_needs_processing = True
-        
-    def setForcedSpeed(self, speed):
-        updated = False
-        
-        if (self.forced_speed_available == True):
-            self.forced_speed = speed
-            self.new_forced_speed = True
-            updated = True
-        return updated
         
     def setAction(self, action, action_value):
         self.action_needs_processing = False
@@ -59,18 +47,24 @@ class MotionThread(threading.Thread):
                 self.Y.restart()
                 self.deired_yaw = self.yaw + self.action_value
                 self.S.stop()
-                
             
             elif (self.action == MOVE):
                 self.Y.stop()
                 self.S.restart()
-                self.desired_displacement = self.displacement + action.value
+                self.desired_displacement = self.displacement + self.action_value
+                self.S.setSetpoint(self.desired_displacement)
             
             elif (self.action == MOVE_HOLD):
                 self.Y.restart()
                 self.desired_heading = self.yaw
                 self.S.restart()
-                self.desired_displacement = self.displacement + action.value
+                self.desired_displacement = self.displacement + self.action_value
+                self.S.setSetpoint(self.desired_displacement)
+                
+            else:
+                print "ERROR: unknown action processed in motionThread.processAction"
+                
+            self.action_needs_processing = False
                 
     
     def run(self):
@@ -84,13 +78,13 @@ class MotionThread(threading.Thread):
                 self.yaw = self.D.yaw_without_drift
                 
             else:
-                print "D returned false in Motion Thread"
+                print "ERROR: D returned false in Motion Thread"
                 
             if (self.E.update() == True):
                 self.displacement = self.E.displacement
                 
             else:
-                print "E returned false in Motion Thread"
+                print "ERROR: E returned false in Motion Thread"
             
             self.processAction()
             
@@ -101,28 +95,20 @@ class MotionThread(threading.Thread):
                 new_steering = True
                 
             else:
-                print "Y returned false in Motion Thread"
+                print "ERROR: Y returned false in Motion Thread"
             
             
             if (self.S.run(self.displacement) == True):
                 self.speed = self.S.output
-                self.new_speed = True
+                new_speed = True
                 
             else:
-                print "S returned false in Motion Thread"
-            
-            
-            if (self.new_forced_speed == True):
-                self.forced_speed_available = False
-                self.speed = self.forced_speed
-                self.forced_speed_available = True
-                new_speed = True
-                self.new_forced_speed = False
+                print "ERROR: S returned false in Motion Thread"
             
             if (new_steering == True or new_speed == True):
                 self.M.setSpeedAndSteering(self.speed, self.steering)
             else:
-                print "speed and steering not set in Motion Thread"
+                print "ERROR: speed and steering not set in Motion Thread"
                 
             
             
