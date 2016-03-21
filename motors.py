689efde01aeb0359fba51eb3f610c0004c0e602e
psc_aw1 <psc_aw1@srobo.org>
2016-03-21 17:10:14 +0000

    added debug method

--- motors.py
+++ motors.py
@@ -20,14 +20,19 @@ def accelerationRestrictor(dt, last_value, desired_value, max_accel):
 class MotorHandler():
     
     def __init__(self, LeftMotor, RightMotor, time_period = 0.01): #100hz default
+        self.name = "MotorHandler"
         self.LeftMotor = LeftMotor
         self.RightMotor = RightMotor
         self.time_period = time_period
         self.last_time = time.time() - self.time_period
+        self.speed = 0
         self.desired_speed = 0
         self.last_speed = 0
+        self.steering = 0
         self.desired_steering = 0
         self.last_steering = 0
+        self.left_power = 0
+        self.right_power = 0
         #self.update()        #dont think this line is needed (not sure why I ever used it)
         
     def update(self):
@@ -36,17 +41,17 @@ class MotorHandler():
         updated = False
         
         if (dt >= self.time_period):
-            speed = accelerationRestrictor(dt, self.last_speed, self.desired_speed, MAX_SPEED_ACCEL)
-            steering = accelerationRestrictor(dt, self.last_steering, self.desired_steering, MAX_STEERING_ACCEL)
+            self.speed = accelerationRestrictor(dt, self.last_speed, self.desired_speed, MAX_SPEED_ACCEL)
+            self.steering = accelerationRestrictor(dt, self.last_steering, self.desired_steering, MAX_STEERING_ACCEL)
             
-            left_speed = mapToLimits((speed - steering), MAX_OUTPUT, MIN_OUTPUT)
-            right_speed = mapToLimits((speed + steering), MAX_OUTPUT, MIN_OUTPUT)
+            self.left_power = mapToLimits((self.speed - self.steering), MAX_OUTPUT, MIN_OUTPUT)
+            self.right_power = mapToLimits((self.speed + self.steering), MAX_OUTPUT, MIN_OUTPUT)
             
-            self.LeftMotor.power = left_speed
-            self.RightMotor.power = right_speed
+            self.LeftMotor.power = self.left_power
+            self.RightMotor.power = self.right_power
             
-            self.last_speed = speed
-            self.last_steering = steering
+            self.last_speed = self.speed
+            self.last_steering = self.steering
             self.last_time = current_time
             updated = True   
         return updated
@@ -66,3 +71,7 @@ class MotorHandler():
         self.desired_speed = mapToLimits(desired_speed, MAX_SPEED, MIN_SPEED)
         self.desired_steering = mapToLimits(desired_steering, MAX_STEERING, MIN_STEERING)
         return self.update()
+        
+    def debug(self):
+        print self.name + ": desired_speed = " + str(self.desired_speed) + ", speed = " + str(self.speed) + ", desired_steering = " + str(self.desired_steering) + ", steering = " + str(self.steering) + ", left_power = " + str(self.left_power) + ", right_power = " + str(self.right_power)
+        
