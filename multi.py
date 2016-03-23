7d42e4694db2004cc5b679beeedeb662d8e3e830
psc_aw1 <psc_aw1@srobo.org>
2016-03-23 22:24:58 +0000

    Commit message

--- multi.py
+++ multi.py
@@ -29,7 +29,9 @@ def displacementByLineApproximation(length, theta_1, theta_2):
 class MotionThread(threading.Thread):
     
     def __init__(self, MpuHandler, YawPid, MotorHandler, DistancePid, EncoderHandler, time_period = 0.01):
+        
         threading.Thread.__init__(self)
+        
         self.name = "MotionThread"
         
         self.steering = 0
@@ -43,9 +45,9 @@ class MotionThread(threading.Thread):
         self.last_distance = 0
         self.desired_distance = 0
         
+        self.location_lock = threading.Lock()
         self.arc_displacement = {'x': 0, 'y': 0}
         self.line_displacement = {'x': 0, 'y': 0}
-        #self.last_displacement_time = 0
         
         self.D = MpuHandler
         self.Y = YawPid
@@ -55,94 +57,86 @@ class MotionThread(threading.Thread):
         
         self.time_period = time_period
         
+        self.offset_lock = threading.Lock()
         self.mpu_yaw_offset = INITIAL_OFFSET
         self.new_mpu_yaw_offset = 0
-        self.new_mpu_yaw_offset_available = False
         
+        self.action_lock = threading.Lock()
         self.action = STILL
         self.action_value = 0
-        self.action_available = True
         self.action_needs_processing = True
         
-    def setMpuYawOffset(self, offset):
+    def setRobotLocation(self, robot_location):
         
-        if (self.new_mpu_yaw_offset_available == True):
-            offset += self.new_mpu_yaw_offset
-        
-        self.new_mpu_yaw_offset = offset
-        self.new_mpu_yaw_offset_available = True
-        
-    def processMpuYawOffset(self):
-        
-        if (self.new_mpu_yaw_offset_available == True):
-            self.mpu_yaw_offset += self.new_mpu_yaw_offset
-            self.new_mpu_yaw_offset_available == False
+        with self.location_lock:
+            self.robot_location = robot_location
+            self.arc_displacement = {'x': 0, 'y': 0}
+            self.line_displacement = {'x': 0, 'y': 0}
+            
+        with self.offset_lock:
+            self.
         
     def setAction(self, action, action_value = 0):
         
-        if (self.action_available == True):
-            self.action_needs_processing = False
+        with self.action_lock:
             self.action = action
             self.action_value = action_value
             self.action_needs_processing = True
-        
-        else: #self.action_available == False
-            print "ERROR: no action set in MotionThread.setAction()"
     
     def processAction(self):
         
-        if (self.action_needs_processing == True):
-            self.action_available = False
+        with self.action_lock:
             
-            if (self.action == STILL):
-                self.Y.stop()
-                self.S.stop()
-                print "New Action: STILL"
-            
-            elif (self.action == TURN):
-                self.Y.restart()
-                self.desired_yaw = self.yaw + self.action_value
-                self.S.stop() 
-                print "New Action: TURN, with value: " + str(self.action_value)
-            
-            elif (self.action == MOVE):
-                self.Y.stop()
-                self.S.restart()
-                self.desired_distance = self.distance + self.action_value
-                self.S.setSetpoint(self.desired_distance)
-                print "New Action: MOVE, with value: " + str(self.action_value)
-            
-            elif (self.action == MOVE_HOLD):
-                self.Y.restart()
-                self.desired_yaw = self.yaw
-                self.S.restart()
-                self.desired_distance = self.distance + self.action_value
-                self.S.setSetpoint(self.desired_distance)
-                print "New Action: MOVE_HOLD, with value: " + str(self.action_value)
+            if (self.action_needs_processing == True):
                 
-            else:
-                print "ERROR: unknown action processed in motionThread.processAction"
-            
-            self.action_available = True                
-            self.action_needs_processing = False
+                if (self.action == STILL):
+                    self.Y.stop()
+                    self.S.stop()
+                    print "New Action: STILL"
+                
+                elif (self.action == TURN):
+                    self.Y.restart()
+                    self.desired_yaw = self.yaw + self.action_value
+                    self.S.stop() 
+                    print "New Action: TURN, with value: " + str(self.action_value)
+                
+                elif (self.action == MOVE):
+                    self.Y.stop()
+                    self.S.restart()
+                    self.desired_distance = self.distance + self.action_value
+                    self.S.setSetpoint(self.desired_distance)
+                    print "New Action: MOVE, with value: " + str(self.action_value)
+                
+                elif (self.action == MOVE_HOLD):
+                    self.Y.restart()
+                    self.desired_yaw = self.yaw
+                    self.S.restart()
+                    self.desired_distance = self.distance + self.action_value
+                    self.S.setSetpoint(self.desired_distance)
+                    print "New Action: MOVE_HOLD, with value: " + str(self.action_value)
+                    
+                else:
+                    print "ERROR: unknown action processed in motionThread.processAction"
+                   
+                self.action_needs_processing = False
             
     def displacementManager(self):
-        
         theta_1 = self.last_yaw
         theta_2 = self.yaw
         length = self.distance - self.last_distance
         
         dline_displacement = displacementByLineApproximation(length, theta_1, theta_2)
-        self.line_displacement['x'] += dline_displacement['x']
-        self.line_displacement['y'] += dline_displacement['y']
         
         if (theta_2 == theta_1): #error case for arc length /0 err
-            darc_displacement = displacementByLineApproximation(length, theta_1, theta_2)
+            darc_displacement = dline_displacement
         else:
             darc_displacement = displacementByArcApproximation(length, theta_1, theta_2)
-            
-        self.arc_displacement['x'] += darc_displacement['x']
-        self.arc_displacement['y'] += darc_displacement['y']
+        
+        with self.location_lock:
+            self.line_displacement['x'] += dline_displacement['x']
+            self.line_displacement['y'] += dline_displacement['y']
+            self.arc_displacement['x'] += darc_displacement['x']
+            self.arc_displacement['y'] += darc_displacement['y']
     
     def debug(self):
         print self.name
@@ -180,8 +174,6 @@ class MotionThread(threading.Thread):
                 
             else:
                 print "ERROR: E returned false in Motion Thread"
-                
-            self.processMpuYawOffset()
             
             self.processAction()
             
