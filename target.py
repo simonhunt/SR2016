31416333db10213506afe30c9bbbff28e0c2d82c
psc_aw1 <psc_aw1@srobo.org>
2016-03-31 13:02:42 +0000

    Commit message

--- target.py
+++ target.py
@@ -6,7 +6,7 @@ from actions import *
 
 class TargetThread(threading.Thread):
     
-    def __init__(self, MotionThread, time_period = 5): #default 10hz
+    def __init__(self, MotionThread, time_period = 1): #default 10hz
         threading.Thread.__init__(self)
         
         self.name = "TargetThread"
@@ -46,6 +46,15 @@ class TargetThread(threading.Thread):
                 else:
                     self.calculatePolar()
                     self.MotionThread.setAction(MOVE_AND_TURN_TO, self.polar_r, self.polar_t)
+                    
+                    while (True):
+                        self.time_to_sleep = mapToLimits(wake_up_time - time.time(), self.time_period, 0)
+                        wake_up_time += self.time_period
+                        time.sleep(self.time_to_sleep) 
+                        self.calculatePolar()
+                        self.MotionThread.setAction(MOVE_AND_TURN_TO_CHANGE, self.polar_r, self.polar_t)
+                        
+                        
         print "Exiting " + self.name
     
     def debug(self):
