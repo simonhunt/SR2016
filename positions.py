21a0a2a4890d107ccab7e6621751a955af035507
psc_aw1 <psc_aw1@srobo.org>
2016-04-03 22:51:33 +0000

    Commit message

--- positions.py
+++ positions.py
@@ -1,28 +1,31 @@
 #POSITIONS
 
+# rotate +ve = roll the cube forwards like a front bike wheel rolling forwards
+# lift +ve = arms up
+# grab +ve = arms in
+# time = time taken to move to the position
+
+
 INITIALISATION = {'rotate': 0, 'lift': 0, 'grab': 0, 'time': 1}
 ARMS_UP_OUT_THE_WAY = {'rotate': 0, 'lift': 100, 'grab': 0, 'time': 1}
 
 ARMS_WIDE_POSITIVE_TURN = {'rotate': - 100, 'lift': - 40, 'grab': - 50, 'time': 1}
-ARMS_WIDE_NEGATIVE_TURN = {'rotate': 100, 'lift': - 40, 'grab': - 50, 'time': 1}
+ARMS_ON_CUBE_POSITIVE_TURN = {'rotate': - 100, 'lift': - 40, 'grab': 30, 'time': 1}
+LIFT_CUBE_POSITIVE_TURN = {'rotate': - 20, 'lift': 40, 'grab': 30, 'time': 1}
+TURN_CUBE_90 = {'rotate': 80, 'lift': 40, 'grab': 30, 'time': 1}
+DOWN_CUBE_90 = {'rotate': 40, 'lift': 0, 'grab': 30, 'time': 1}
+RELEASE_CUBE_90 = {'rotate': 40, 'lift': 0, 'grab': 0, 'time': 1}
 
-ARMS_ON_CUBE_POSITIVE_TURN = {'rotate': - 100, 'lift': - 40, 'grab': 20, 'time': 1}
-ARMS_ON_CUBE_NEGATIVE_TURN = {'rotate': 100, 'lift': - 40, 'grab': 20, 'time': 1}
+ARMS_WIDE_NEGATIVE_TURN = {'rotate': 20, 'lift': - 40, 'grab': - 50, 'time': 1}
+ARMS_ON_CUBE_NEGATIVE_TURN = {'rotate': 20, 'lift': - 40, 'grab': 30, 'time': 1}
+LIFT_CUBE_NEGATIVE_TURN = {'rotate': 100, 'lift': 40, 'grab': 30, 'time': 1}
+TURN_CUBE_MINUS_90 = {'rotate': 0, 'lift': 40, 'grab': 30, 'time': 1}
+DOWN_CUBE_MINUS_90 = {'rotate': - 40, 'lift': 0, 'grab': 30, 'time': 1}
+RELEASE_CUBE_MINUS_90 = {'rotate': - 40, 'lift': 0, 'grab': 0, 'time': 1}
 
-LIFT_CUBE_POSITIVE_TURN = {'rotate': - 100, 'lift': 40, 'grab': 20, 'time': 1}
-LIFT_CUBE_NEGATIVE_TURN = {'rotate': 100, 'lift': 40, 'grab': 20, 'time': 1}
-
-TURN_CUBE_90 = {'rotate': 0, 'lift': 40, 'grab': 20, 'time': 1}
-TURN_CUBE_180 = {'rotate': 100, 'lift': 40, 'grab': 20, 'time': 1}
-TURN_CUBE_MINUS_90 = {'rotate': 0, 'lift': 40, 'grab': 20, 'time': 1}
-TURN_CUBE_MINUS_180 = {'rotate': - 100, 'lift': 40, 'grab': 20, 'time': 1}
-
-DOWN_CUBE_90 = {'rotate': 0, 'lift': 0, 'grab': 20, 'time': 1}
-DOWN_CUBE_180 = {'rotate': 100, 'lift': 0, 'grab': 20, 'time': 1}
-DOWN_CUBE_MINUS_90 = {'rotate': 0, 'lift': 0, 'grab': 20, 'time': 1}
-DOWN_CUBE_MINUS_180 = {'rotate': - 100, 'lift': 0, 'grab': 20, 'time': 1}
-
-RELEASE_CUBE_90 = {'rotate': 0, 'lift': 0, 'grab': 0, 'time': 1}
-RELEASE_CUBE_180 = {'rotate': 100, 'lift': 0, 'grab': 0, 'time': 1}
-RELEASE_CUBE_MINUS_90 = {'rotate': 0, 'lift': 0, 'grab': 0, 'time': 1}
-RELEASE_CUBE_MINUS_180 = {'rotate': - 100, 'lift': 0, 'grab': 0, 'time': 1}
+ARMS_WIDE_NEGATIVE_TURN = {'rotate': 60, 'lift': - 40, 'grab': - 50, 'time': 1}
+ARMS_ON_CUBE_NEGATIVE_TURN = {'rotate': 60, 'lift': - 40, 'grab': 30, 'time': 1}
+LIFT_CUBE_NEGATIVE_TURN = {'rotate': 100, 'lift': 40, 'grab': 30, 'time': 1}
+TURN_CUBE_MINUS_180 = {'rotate': -60, 'lift': 40, 'grab': 30, 'time': 1}
+DOWN_CUBE_MINUS_180 = {'rotate': -100, 'lift': 0, 'grab': 30, 'time': 1}
+RELEASE_CUBE_MINUS_180 = {'rotate': -100, 'lift': 0, 'grab': 0, 'time': 1}
