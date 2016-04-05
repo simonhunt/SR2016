d10bd90af37e07e5dac35b9b837dda492f5a1d0f
psc_aw1 <psc_aw1@srobo.org>
2016-04-05 19:10:03 +0000

    Commit message

--- power.py
+++ power.py
@@ -1,6 +1,18 @@
-NOTE = 'a'
-DURATION = 500 #milliseconds
+READY_NOTE = 'c'
+READY_DURATION = 500 #milliseconds
+
+GOOD_NOTE = 'g'
+GOOD_DURATION = 1000 #milliseconds
+
+BAD_NOTE = 'a'
+BAD_DURATION = 1500 #milliseconds
 
 def signalReady(Power):
-    Power.beep(DURATION, note = NOTE)
+    Power.beep(READY_DURATION, note = READY_NOTE)
+    
+def signalGood(Power):
+    Power.beep(GOOD_DURATION, note = GOOD_NOTE)
+
+def signalBad(Power):
+    Power.beep(BAD_DURATION, note = BAD_NOTE
     
