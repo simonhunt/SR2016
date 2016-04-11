2b2f7408586d0da432b91e624e82a414710cae01
psc_aw1 <psc_aw1@srobo.org>
2016-04-11 15:51:47 +0000

    Commit message

--- positions.py
+++ positions.py
@@ -9,9 +9,9 @@
 INITIALISATION = {'rotate': 0, 'lift': 0, 'grab': 0, 'time': 1}
 ARMS_UP_OUT_THE_WAY = {'rotate': 0, 'lift': 100, 'grab': 0, 'time': 1}
 
-ARMS_WIDE_ZERO= {'rotate': 0, 'lift': - 40, 'grab': - 50, 'time': 1}
+ARMS_WIDE_ZERO = {'rotate': 0, 'lift': - 40, 'grab': - 50, 'time': 1}
 ARMS_ON_CUBE_ZERO = {'rotate': 0, 'lift': - 40, 'grab': 35, 'time': 2}
-HOLD_CUBE_ZERO = {'rotate': 0, 'lift': 0, 'grab': 35, 'time': 1}
+LIFT_CUBE_ZERO = {'rotate': 0, 'lift': 0, 'grab': 35, 'time': 1}
 RELEASE_CUBE_ZERO = {'rotate': 0, 'lift': 0, 'grab': 0, 'time': 0.5}
 
 ARMS_WIDE_90= {'rotate': - 100, 'lift': - 50, 'grab': - 50, 'time': 1}
@@ -28,7 +28,6 @@ TURN_CUBE_NEGATIVE_90 = {'rotate': - 90, 'lift': 40, 'grab': 35, 'time': 2}
 DOWN_CUBE_NEGATIVE_90 = {'rotate': - 90, 'lift': 0, 'grab': 35, 'time': 1}
 RELEASE_CUBE_NEGATIVE_90 = {'rotate': - 90, 'lift': 0, 'grab': 0, 'time': 0.5}
 
-
 ARMS_ON_CUBE_180 = {'rotate': 100, 'lift': - 40, 'grab': 35, 'time': 2}
 LIFT_CUBE_180 = {'rotate': 100, 'lift': 40, 'grab': 35, 'time': 2}
 TURN_CUBE_180 = {'rotate': - 100, 'lift': 40, 'grab': 35, 'time': 2}
@@ -37,11 +36,7 @@ RELEASE_CUBE_180 = {'rotate': - 100, 'lift': 0, 'grab': 0, 'time': 0.5}
 PREPARE_SECOND_ROTATE_180 = {'rotate': 100, 'lift': - 40, 'grab': 0, 'time': 0.5}
 ARMS_ON_CUBE_180_FAST = {'rotate': 100, 'lift': - 40, 'grab': 35, 'time': 0.5}
 
-SKANK_1 = {'rotate': 100, 'lift': 100, 'grab': - 100, 'time': 1}
-SKANK_2 = {'rotate': - 100, 'lift': - 50, 'grab': 10, 'time': 1}
-
-TEST_SKANK = [SKANK_1, SKANK_2, SKANK_1, SKANK_2, SKANK_1, SKANK_2, SKANK_1, SKANK_2, SKANK_1, SKANK_2, SKANK_1, SKANK_2, SKANK_1, SKANK_2, SKANK_1, SKANK_2, SKANK_1, SKANK_2,]
-
+TEST_SEQUENCE_ZERO = [ARMS_WIDE_ZERO, ARMS_ON_CUBE_ZERO, LIFT_CUBE_ZERO, RELEASE_CUBE_ZERO]
 TEST_SEQUENCE_90 = [ARMS_WIDE_90, ARMS_ON_CUBE_90, LIFT_CUBE_90, TURN_CUBE_90, DOWN_CUBE_90, RELEASE_CUBE_90, ARMS_UP_OUT_THE_WAY]
 TEST_SEQUENCE_NEGATIVE_90 = [ARMS_WIDE_NEGATIVE, ARMS_ON_CUBE_NEGATIVE_90, LIFT_CUBE_NEGATIVE_90, TURN_CUBE_NEGATIVE_90, DOWN_CUBE_NEGATIVE_90, RELEASE_CUBE_NEGATIVE_90, ARMS_UP_OUT_THE_WAY]
 TEST_SEQUENCE_180 = [ARMS_WIDE_NEGATIVE, ARMS_ON_CUBE_180, LIFT_CUBE_180, TURN_CUBE_180, DOWN_CUBE_180, RELEASE_CUBE_180, PREPARE_SECOND_ROTATE_180, ARMS_ON_CUBE_180_FAST, LIFT_CUBE_180, TURN_CUBE_NEGATIVE_90, DOWN_CUBE_NEGATIVE_90, RELEASE_CUBE_NEGATIVE_90, ARMS_UP_OUT_THE_WAY]
