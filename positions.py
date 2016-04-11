3662760d2f6755cf6b796dba5eb4ef39f3c3f8e1
psc_aw1 <psc_aw1@srobo.org>
2016-04-11 15:44:38 +0000

    Commit message

--- positions.py
+++ positions.py
@@ -9,6 +9,11 @@
 INITIALISATION = {'rotate': 0, 'lift': 0, 'grab': 0, 'time': 1}
 ARMS_UP_OUT_THE_WAY = {'rotate': 0, 'lift': 100, 'grab': 0, 'time': 1}
 
+ARMS_WIDE_ZERO= {'rotate': 0, 'lift': - 40, 'grab': - 50, 'time': 1}
+ARMS_ON_CUBE_ZERO = {'rotate': 0, 'lift': - 40, 'grab': 35, 'time': 2}
+HOLD_CUBE_ZERO = {'rotate': 0, 'lift': 0, 'grab': 35, 'time': 1}
+RELEASE_CUBE_ZERO = {'rotate': 0, 'lift': 0, 'grab': 0, 'time': 0.5}
+
 ARMS_WIDE_90= {'rotate': - 100, 'lift': - 50, 'grab': - 50, 'time': 1}
 ARMS_ON_CUBE_90 = {'rotate': - 100, 'lift': - 50, 'grab': 35, 'time': 2}
 LIFT_CUBE_90 = {'rotate': - 100, 'lift': 60, 'grab': 35, 'time': 2}
@@ -23,13 +28,14 @@ TURN_CUBE_NEGATIVE_90 = {'rotate': - 90, 'lift': 40, 'grab': 35, 'time': 2}
 DOWN_CUBE_NEGATIVE_90 = {'rotate': - 90, 'lift': 0, 'grab': 35, 'time': 1}
 RELEASE_CUBE_NEGATIVE_90 = {'rotate': - 90, 'lift': 0, 'grab': 0, 'time': 0.5}
 
+
 ARMS_ON_CUBE_180 = {'rotate': 100, 'lift': - 40, 'grab': 35, 'time': 2}
 LIFT_CUBE_180 = {'rotate': 100, 'lift': 40, 'grab': 35, 'time': 2}
 TURN_CUBE_180 = {'rotate': - 100, 'lift': 40, 'grab': 35, 'time': 2}
 DOWN_CUBE_180 = {'rotate': - 100, 'lift': -35, 'grab': 35, 'time': 1}
 RELEASE_CUBE_180 = {'rotate': - 100, 'lift': 0, 'grab': 0, 'time': 0.5}
 PREPARE_SECOND_ROTATE_180 = {'rotate': 100, 'lift': - 40, 'grab': 0, 'time': 0.5}
-ARMS_ON_CUBE_180_FAST = {'rotate': 100, 'lift': - 40, 'grab': 35, 'time': 1}
+ARMS_ON_CUBE_180_FAST = {'rotate': 100, 'lift': - 40, 'grab': 35, 'time': 0.5}
 
 SKANK_1 = {'rotate': 100, 'lift': 100, 'grab': - 100, 'time': 1}
 SKANK_2 = {'rotate': - 100, 'lift': - 50, 'grab': 10, 'time': 1}
