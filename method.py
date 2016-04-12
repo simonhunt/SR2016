e12acfef7c44a44a7f19f6d4d3176fba42ad484d
psc_aw1 <psc_aw1@srobo.org>
2016-04-12 21:24:07 +0000

    Commit message

--- method.py
+++ method.py
@@ -4,7 +4,6 @@ def decideCubeToApproach(a_cube_locations, b_cube_locations, c_cube_locations):
     selected_approach_location = selected_cube['approach'][0].deepcopy()
     selected_cube_net = 'A'
     
-    
     cube_approach = {'x': selected_cube['x'], 'y': selected_cube['y'], 'z': selected_cube['z'], 'yaw': selected_cube['yaw'], 'pitch': selected_cube['pitch'], 'roll': selected_cube['roll']}
     cube_approach['approach_location'] = selected_approach_location
     cube_approach['net'] = selected_cube_net
