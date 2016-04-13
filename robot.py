669c8b0385573a3c271ae9c8997008f3cd1f333a
psc_aw1 <psc_aw1@srobo.org>
2016-04-13 10:01:06 +0000

    Commit message

--- robot.py
+++ robot.py
@@ -167,21 +167,21 @@ if (DEBUG_YAW_DRIFT == True):
 
 #TargetThread.setTarget(test_target)
 
-#ServoThread.setPosition(ARMS_WIDE_ZERO)
-#
-#TargetThread.addTarget(test_target_1)
-#
-#time.sleep(10)
-#
-#TargetThread.addTarget(test_target_2)
-#
-#time.sleep(10)
-#
-#TargetThread.addTarget(test_target_3)
-#
-#time.sleep(10)
-#
-#TargetThread.addTarget(origin)
+ServoThread.setPosition(ARMS_WIDE_ZERO)
+
+TargetThread.addTarget(test_target_1)
+
+time.sleep(10)
+
+TargetThread.addTarget(test_target_2)
+
+time.sleep(10)
+
+TargetThread.addTarget(test_target_3)
+
+time.sleep(10)
+
+TargetThread.addTarget(origin)
 
 
 
