c7516cb82b52060b35311ae494a9feaefc4ecb0d
psc_aw1 <psc_aw1@srobo.org>
2016-04-16 14:30:37 +0000

    Commit message

--- positions.py
+++ positions.py
@@ -15,10 +15,10 @@ LIFT_CUBE_ZERO = {'rotate': 0, 'lift': 0, 'grab': 35, 'time': 1}
 RELEASE_CUBE_ZERO = {'rotate': 0, 'lift': 0, 'grab': 0, 'time': 0.5}
 
 ARMS_WIDE_90= {'rotate': - 100, 'lift': - 100, 'grab': - 70, 'time': 1}
-ARMS_ON_CUBE_90 = {'rotate': - 100, 'lift': - 100, 'grab': 10, 'time': 2}
-LIFT_CUBE_90 = {'rotate': - 100, 'lift': 60, 'grab': 10, 'time': 2}
-TURN_CUBE_90 = {'rotate': 100, 'lift': 60, 'grab': 10, 'time': 2}
-DOWN_CUBE_90 = {'rotate': 100, 'lift': -70, 'grab': 10, 'time': 1}
+ARMS_ON_CUBE_90 = {'rotate': - 100, 'lift': - 100, 'grab': 20, 'time': 2}
+LIFT_CUBE_90 = {'rotate': - 100, 'lift': 60, 'grab': 20, 'time': 2}
+TURN_CUBE_90 = {'rotate': 100, 'lift': 60, 'grab': 20, 'time': 2}
+DOWN_CUBE_90 = {'rotate': 100, 'lift': -70, 'grab': 20, 'time': 1}
 RELEASE_CUBE_90 = {'rotate': 100, 'lift': -70, 'grab': -20, 'time': 0.5}
 
 ARMS_WIDE_NEGATIVE= {'rotate': 100, 'lift': - 40, 'grab': - 50, 'time': 1}
