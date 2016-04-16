abf6705c2694f890c5d39c60a31b8cb4d16cf218
psc_aw1 <psc_aw1@srobo.org>
2016-04-16 20:20:51 +0000

    polar angle maths funcitons

---
+++ polar.py
@@ -0,0 +1,9 @@
+def getPolarR(start_location, finish_location):
+    dx = self.start_location['x'] - finish_location['x']
+    dy = self.start_location['y'] - finish_location['y']
+    polar_r = (dx**2 + dy**2)**0.5
+    
+def getPolarT(start_location, finish_location):
+    dx = self.start_location['x'] - finish_location['x']
+    dy = self.start_location['y'] - finish_location['y']
+    self.polar_t = math.degrees(math.atan2(dy, dx))
