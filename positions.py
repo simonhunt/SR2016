01ecf9290cb1695807bbacc31c2c40609c3da6c5
psc_aw1 <psc_aw1@srobo.org>
2016-04-16 14:52:15 +0000

    Commit message

--- positions.py
+++ positions.py
@@ -25,8 +25,8 @@ ARMS_WIDE_NEGATIVE= {'rotate': 100, 'lift': - 100, 'grab': - 70, 'time': 1}
 ARMS_ON_CUBE_NEGATIVE_90 = {'rotate': 100, 'lift': - 100, 'grab': 20, 'time': 2}
 LIFT_CUBE_NEGATIVE_90 = {'rotate': 100, 'lift': 30, 'grab': 20, 'time': 2}
 TURN_CUBE_NEGATIVE_90 = {'rotate': -20, 'lift': 30, 'grab': 20, 'time': 2}
-DOWN_CUBE_NEGATIVE_90 = {'rotate': -70, 'lift': -50, 'grab': 20, 'time': 1}
-RELEASE_CUBE_NEGATIVE_90 = {'rotate': -70, 'lift': -50, 'grab': -20, 'time': 0.5}
+DOWN_CUBE_NEGATIVE_90 = {'rotate': -90, 'lift': -70, 'grab': 20, 'time': 1}
+RELEASE_CUBE_NEGATIVE_90 = {'rotate': -90, 'lift': -70, 'grab': -20, 'time': 0.5}
 
 ARMS_ON_CUBE_180 = {'rotate': 100, 'lift': - 40, 'grab': 35, 'time': 2}
 LIFT_CUBE_180 = {'rotate': 100, 'lift': 40, 'grab': 35, 'time': 2}
