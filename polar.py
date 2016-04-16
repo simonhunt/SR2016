5ac079e9cdfcbaacd7de75cd16cda33275f800a6
psc_aw1 <psc_aw1@srobo.org>
2016-04-16 20:25:43 +0000

    Commit message

--- polar.py
+++ polar.py
@@ -1,12 +1,12 @@
 import math
 
 def getPolarR(start_location, finish_location):
-    dx = start_location['x'] - finish_location['x']
-    dy = start_location['y'] - finish_location['y']
+    dx = finish_location['x'] - start_location['x']
+    dy = finish_location['y'] - start_location['y']
     polar_r = (dx**2 + dy**2)**0.5
     
 def getPolarT(start_location, finish_location):
-    dx = start_location['x'] - finish_location['x']
-    dy = start_location['y'] - finish_location['y']
+    dx = finish_location['x'] - start_location['x']
+    dy = finish_location['y'] - start_location['y']
     polar_t = math.degrees(math.atan2(dy, dx))
     
