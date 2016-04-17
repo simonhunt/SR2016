bb9a74c33bb56c3f97e08ca4c0c3e5549cba8c60
psc_aw1 <psc_aw1@srobo.org>
2016-04-17 12:15:25 +0000

    added getTeamFacingUp

--- turn.py
+++ turn.py
@@ -2,6 +2,52 @@ from sr.robot import NET_A, NET_B, NET_C, MARKER_TOKEN_SIDE, MARKER_TOKEN_TOP, M
 from lut import TURN_TABLE
 from limits import mod4
 
+def getTeamFacingUp(net, roll, code):
+    
+    if (roll <= 45 and roll > -45):
+        roll_index = 0 #a
+        
+    elif (roll <= -45 and roll > -135):
+        roll_index = 1 #b
+    
+    elif (roll <= 135 and roll > 45):
+        roll_index = 3 #d
+    
+    else:
+        roll_index = 2 #c
+    
+    if (net == NET_A):
+        net_index = 0
+        side_index = code - 34
+        
+    elif (net == NET_B):
+        net_index = 1
+        side_index = code - 40
+        
+    elif (net == NET_C):
+        net_index = 2
+        side_index = code - 46
+        
+    else:
+        print "error marker with undefined net handled in getTeamFacingUp"
+        
+        #defaults
+        
+        net_index = 0 
+        side_index = code - 34 
+    
+    zone_indexes = [0, 1 ,2, 3]
+    
+    team = None
+    
+    for zone_index in zone_indexes:
+        table_output = TURN_TABLE[net_index][side_index][roll_index][zone_index]
+        
+        if (table_output[1] == 0):
+            team = zone_index
+    
+    return team
+
 def getTurns(net, roll, code, zone):
     
     if (roll <= 45 and roll > -45):
@@ -39,7 +85,7 @@ def getTurns(net, roll, code, zone):
         side_index = code - 46
         
     else:
-        print "error marker with undefined net handled in turn handler"
+        print "error marker with undefined net handled in getTurns"
         
         #defaults
         
