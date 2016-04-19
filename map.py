c8879498af14e60bdd3b5dc90aae32b03edb93d7
psc_aw1 <psc_aw1@srobo.org>
2016-04-19 11:52:11 +0000

    Commit message

--- map.py
+++ map.py
@@ -9,12 +9,12 @@ from limits import mapToLimits, angleMod
 ROBOT_WIDTH = 0.5
 TOKEN_WIDTH = 0.25
 
-ROBOT_TO_CAMERA_VECTOR = {'alpha': 0, 'beta': 0, 'gamma': 0.255} #alpha forwards, beta left to right, gamma up
+ROBOT_TO_CAMERA_VECTOR = {'alpha': 0, 'beta': 0, 'gamma': 0} #alpha forwards, beta left to right, gamma up
 ROBOT_TO_CAMERA_YAW = 0
-ROBOT_TO_CAMERA_PITCH = -11 #deg
-ROBOT_TO_CAMERA_ROLL = 1.7 #deg
+ROBOT_TO_CAMERA_PITCH = 0# -11 #deg
+ROBOT_TO_CAMERA_ROLL = 0 #1.7 #deg
 
-ZONE_0_INITIAL_CAMERA_LOCATION = {'x': 1, 'y': 7, 'z': 0.38, 'yaw': -45, 'pitch': 11, 'roll': 1.7, 'time': None}
+ZONE_0_INITIAL_CAMERA_LOCATION = {'x': 1, 'y': 7, 'z': 0.125, 'yaw': -45, 'pitch': 0, 'roll': 0, 'time': None}
 ZONE_1_INITIAL_CAMERA_LOCATION = {'x': 7, 'y': 7, 'z': 0.125, 'yaw': -135, 'pitch': 0, 'roll': 0, 'time': None}
 ZONE_2_INITIAL_CAMERA_LOCATION = {'x': 7, 'y': 1, 'z': 0.125, 'yaw': 135, 'pitch': 0, 'roll': 0, 'time': None}
 ZONE_3_INITIAL_CAMERA_LOCATION = {'x': 1, 'y': 1, 'z': 0.125, 'yaw': 45, 'pitch': 0, 'roll': 0, 'time': None}
