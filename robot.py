41a6b035b50521b9edf81a13a5598f708d610990
psc_aw1 <psc_aw1@srobo.org>
2016-04-19 09:31:20 +0000

    Commit message

--- robot.py
+++ robot.py
@@ -228,7 +228,11 @@ def testArms():
         ServoThread.addSequence(TEST_SEQUENCE_180)
         time.sleep(30)
 
-time.sleep(30)
+time.sleep(10)
+
+MotionThread.setAction(TURN_TO, - 45)
+
+time.sleep(10)
 
 power.signalGood(R.power)
 
