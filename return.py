c6a7a63e6e2a57d069c0cc7d315ac3d0658f5434
psc_aw1 <psc_aw1@srobo.org>
2016-04-19 20:54:53 +0000

    Commit message

---
+++ return.py
@@ -0,0 +1,29 @@
+
+
+ZONE_0_STORE_LOCATION_0 = {'x': 0.5, 'y': 7.5}
+ZONE_0_STORE_LOCATION_1 = {'x': 1, 'y': 7.5}
+ZONE_0_STORE_LOCATION_2 = {'x': 0.5, 'y': 7}
+ZONE_0_STORE_LOCATION_3 = {'x': 1, 'y': 7}
+
+ZONE_1_STORE_LOCATION_0 = {'x': 7.5, 'y': 7.5}
+ZONE_1_STORE_LOCATION_1 = {'x': 7.5, 'y': 7}
+ZONE_1_STORE_LOCATION_2 = {'x': 7, 'y': 7.5}
+ZONE_1_STORE_LOCATION_3 = {'x': 7, 'y': 7}
+
+ZONE_2_STORE_LOCATION_0 = {'x': 7.5, 'y': 0.5}
+ZONE_2_STORE_LOCATION_1 = {'x': 7, 'y': 0.5}
+ZONE_2_STORE_LOCATION_2 = {'x': 7.5, 'y': 1}
+ZONE_2_STORE_LOCATION_3 = {'x': 7, 'y': 1}
+
+ZONE_3_STORE_LOCATION_0 = {'x': 0.5, 'y': 0.5}
+ZONE_3_STORE_LOCATION_1 = {'x': 0.5, 'y': 1}
+ZONE_3_STORE_LOCATION_2 = {'x': 1, 'y': 0.5}
+ZONE_3_STORE_LOCATION_3 = {'x': 1, 'y': 1}
+
+
+
+
+class ReturnManager():
+    
+    def __innit__(self, zone):
+        
