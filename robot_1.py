6f65cdf3e58083ab17655695764ed4a8ae96c5c6
psc_aw1 <psc_aw1@srobo.org>
2016-04-19 17:19:18 +0000

    Commit message

---
+++ robot_1.py
@@ -0,0 +1,14 @@
+#actions
+
+#debug
+
+#limits
+
+#lut
+
+#map
+
+ZONE_0_INITIAL_CAMERA_LOCATION = {'x': 1, 'y': 7, 'z': 0.125, 'yaw': -45, 'pitch': 0, 'roll': 0, 'time': None}
+ZONE_1_INITIAL_CAMERA_LOCATION = {'x': 7, 'y': 7, 'z': 0.125, 'yaw': -135, 'pitch': 0, 'roll': 0, 'time': None}
+ZONE_2_INITIAL_CAMERA_LOCATION = {'x': 7, 'y': 1, 'z': 0.125, 'yaw': 135, 'pitch': 0, 'roll': 0, 'time': None}
+ZONE_3_INITIAL_CAMERA_LOCATION = {'x': 1, 'y': 1, 'z': 0.125, 'yaw': 45, 'pitch': 0, 'roll': 0, 'time': None}
