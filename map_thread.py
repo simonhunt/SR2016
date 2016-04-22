daec5970c03ad2439394a90563a41c6850e34ff3
psc_aw1 <psc_aw1@srobo.org>
2016-04-22 09:56:23 +0000

    Commit message

--- map_thread.py
+++ map_thread.py
@@ -29,7 +29,7 @@ class MapThread(threading.Thread):
         threading.Thread.__init__(self)
         self.name = "MapThread"
         self.servos = servos
-        self.camera_angle = 0
+        self.camera_angle = 6
         self.power = power
         self.a_cube_locations = []
         self.b_cube_locations = []
