9885783ea7176915222003dc60dddfc8b6253746
psc_aw1 <psc_aw1@srobo.org>
2016-04-22 09:09:20 +0000

    Commit message

--- robot.py
+++ robot.py
@@ -20,7 +20,7 @@ from limits import mapToLimits
 from actions import *
 
 
-from robot_1 import YAW_DRIFT, YKP, YKI, YKD, Y_I_LIMIT, SKP, SKI, SKD, S_I_LIMIT, MAX_STEERING, MAX_SPEED, MIN_SPEED, CAMERA_SERVO_BOARD, CAMERA_SERVO_PIN
+from robot_1 import YAW_DRIFT, YKP, YKI, YKD, Y_I_LIMIT, SKP, SKI, SKD, S_I_LIMIT, MAX_STEERING, MAX_SPEED, MIN_SPEED
 
 from sr.robot import *
 
