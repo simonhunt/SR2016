24885ba9faf68bcff317b37656c351da9516019e
psc_aw1 <psc_aw1@srobo.org>
2016-04-22 22:16:55 +0000

    Commit message

--- steady_thread.py
+++ steady_thread.py
@@ -4,82 +4,91 @@ import time
 import noise
 
 from limits import mapToLimits
+from debug import DEBUG_STEADYCAM
 
-from robot_1 import CAMERA_SERVO_BOARD, CAMERA_SERVO_PIN
+from robot_1 import MAX_CAMERA_ANGLE, MIN_CAMERA_ANGLE, CAMERA_TURN_RATE, CAMERA_STABILISATION_TIME
 
-#temp consts
-MAX_CAMERA_OUTPUT = 0
-MIN_CAMERA_OUTPUT = 180
-MID_CAMERA_ANGLE = 6
-MAX_CAMERA_ANGLE = 180 #deg
-MIN_CAMERA_ANGLE = 0 #deg
-CAMERA_TURN_RATE = 180 #deg/sec
+MAX_CAMERA_OUTPUT = 180
+MIN_CAMERA_OUTPUT = 0
+# MAX_CAMERA_ANGLE = 180 #deg
+# MIN_CAMERA_ANGLE = 0 #deg
+# CAMERA_TURN_RATE = 180 #output/sec
+# CAMERA_STABILISATION_TIME = 0.1 #sec
+CAMERA_PAN_SEQUENCE = [0.5, 0.75, 1, 0.75, 0.5, 0.25, 0, 0.25]
+START_PAN_INDEX = 0
 
 class SteadycamThread(threading.Thread):
     
-    def __init__(self, ruggeduino, power):        
+    def __init__(self, ruggeduino, power, steady_time_period = 0.01, pan_time_period = 0.1): #100hz, 10hz
         threading.Thread.__init__(self)
         self.name = "Steadycam"
-        self.last_output = 0
         self.ruggeduino = ruggeduino
-        self.camera_angle = MID_CAMERA_ANGLE
         self.power = power
+        self.steady_time_period = steady_time_period
+        self.pan_time_period = pan_time_period
+        self.camera_angle = MIN_CAMERA_ANGLE
+        self.last_output = 0
+        self.steady_targets = None
+        self.next_pan = False
+        self.current_pan_index = START_PAN_INDEX
+        self.camera_moving_lock = threading.Lock()
         
-        self.changeCameraAngle()
-        self.changeCameraAngle()
-        self.changeCameraAngle()
-        self.changeCameraAngle()
-        self.changeCameraAngle()
-        self.changeCameraAngle()
+        self.setPan()
         
-    def changeCameraAngle(self):
-        
-        
-        
-        if (self.camera_angle == MID_CAMERA_ANGLE):
-            self.servos[CAMERA_SERVO_BOARD][CAMERA_SERVO_PIN] = 100
-            self.ruggeduino.setCameraServoAngle(180)
-            time.sleep(0.5)
-            self.camera_angle = MAX_CAMERA_ANGLE
-            print "changed to 100"
-        
-        elif (self.camera_angle == MAX_CAMERA_ANGLE):
-            self.servos[CAMERA_SERVO_BOARD][CAMERA_SERVO_PIN] = -100
-            self.ruggeduino.setCameraServoAngle(0)
-            time.sleep(1)
-            self.camera_angle = MIN_CAMERA_ANGLE
-            print "changed to -100"
-        
-        elif (self.camera_angle == MIN_CAMERA_ANGLE):
-            self.servos[CAMERA_SERVO_BOARD][CAMERA_SERVO_PIN] = 0
-            self.ruggeduino.setCameraServoAngle(90)
-            time.sleep(0.5)
-            self.camera_angle = MID_CAMERA_ANGLE
-            print "changed to 0"
-            
+    def nextPan(self):
+        self.steady_targets = None
+        self.next_pan = True
         
     def moveCameraServo(self, new_camera_angle):
-        
-        new_camera_angle = mapToLimits(new_camera_angle, MIN_CAMERA_ANGLE, MAX_CAMERA_ANGLE)]
-        new_output = mapToLimits(((((new_camera_angle - MIN_CAMERA_ANGLE) / (MAX_CAMERA_ANGLE - MIN_CAMERA_ANGLE)) * (MAX_CAMERA_OUTPUT - MIN_CAMERA_OUTPUT)) + MIN_CAMERA_OUTPUT), MAX_CAMERA_OUTPUT, MIN_CAMERA_OUTPUT)
-        self.ruggeduino.setCameraServoAngle(180)
-        
+        new_camera_angle = mapToLimits(new_camera_angle, MIN_CAMERA_ANGLE, MAX_CAMERA_ANGLE)
+        new_output = int(mapToLimits(((((new_camera_angle - MIN_CAMERA_ANGLE) / (MAX_CAMERA_ANGLE - MIN_CAMERA_ANGLE)) * (MAX_CAMERA_OUTPUT - MIN_CAMERA_OUTPUT)) + MIN_CAMERA_OUTPUT), MAX_CAMERA_OUTPUT, MIN_CAMERA_OUTPUT))
+        change_in_output = abs(new_output - self.last_output)
+        self.ruggeduino.setCameraServoAngle(new_output)
+        self.camera_angle = new_camera_angle
+        return change_in_output
     
     def run(self):
         print "Starting " + self.name
         
         while (True):
             
+            if (self.steady_targets == None):
+                self.panBehaviour()
+                time.sleep(self.pan_time_period)
+                
+            else: # self.steady_targets != None
+                self.steadyBehaviour()
+                time.sleep(self.steady_time_period)
         
         print "Exiting " + self.name
         
+    def panBehaviour(self):
+        
+        if (self.next_pan == True):
+            
+            self.next_pan = False
+            self.pan_index = (self.current_pan_index + 1) % len(CAMERA_PAN_SEQUENCE)
+            
+            with self.camera_moving_lock:
+                self.setPan()
+            
+    def setPan(self):
+        pan_fraction = CAMERA_PAN_SEQUENCE[self.pan_index]
+        new_camera_angle = ((MAX_CAMERA_ANGLE - MIN_CAMERA_ANGLE) * pan_fraction) + MIN_CAMERA_ANGLE
+        change_in_output = self.moveCameraServo(new_camera_angle)
+        time_to_sleep = (change_in_output / CAMERA_TURN_RATE) + CAMERA_STABILISATION_TIME
+        time.sleep(time_to_sleep)
+    
+    def steadyBehaviour(self):
+        pass
+        
     def debug(self):
         
-        if (DEBUG_MAP == True):
+        if (DEBUG_STEADYCAM == True):
             
             print self.name
             
-            print "length of a_cube_locations = " + str(len(self.a_cube_locations))
-            print "length of b_cube_locations = " + str(len(self.b_cube_locations))
-            print "length of c_cube_locations = " + str(len(self.c_cube_locations))        
+            print "camera_angle = " + str(self.camera_angle) + ", last_output = " + str(self.last_output)
+            print "steady_targets = " + str(self.steady_targets)
+                    
         
