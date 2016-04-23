a67346a2cf9c26a5031b616b56fcc8ba33eda0ab
psc_aw1 <psc_aw1@srobo.org>
2016-04-23 10:40:50 +0000

    Commit message

--- robot_1.py
+++ robot_1.py
@@ -13,10 +13,10 @@ ENCODER_RESOLUTION = 96
 
 #map
 
-ZONE_0_INITIAL_CAMERA_LOCATION = {'x': 1, 'y': 7, 'z': 0.125 + 0.315, 'yaw': - 45, 'pitch': - 13.6, 'roll': 2, 'time': None}
-ZONE_1_INITIAL_CAMERA_LOCATION = {'x': 7, 'y': 7, 'z': 0.125 + 0.315, 'yaw': - 135, 'pitch': - 13.6, 'roll': 2, 'time': None}
-ZONE_2_INITIAL_CAMERA_LOCATION = {'x': 7, 'y': 1, 'z': 0.125 + 0.315, 'yaw': 135, 'pitch': - 13.6, 'roll': 2, 'time': None}
-ZONE_3_INITIAL_CAMERA_LOCATION = {'x': 1, 'y': 1, 'z': 0.125 + 0.315, 'yaw': 45, 'pitch': - 13.6, 'roll': 2, 'time': None}
+ZONE_0_INITIAL_CAMERA_LOCATION = {'x': 1, 'y': 7, 'z': 0.125 + 0.315, 'yaw': - 45 - 90, 'pitch': - 13.6, 'roll': 2, 'time': None}
+ZONE_1_INITIAL_CAMERA_LOCATION = {'x': 7, 'y': 7, 'z': 0.125 + 0.315, 'yaw': - 135 - 90, 'pitch': - 13.6, 'roll': 2, 'time': None}
+ZONE_2_INITIAL_CAMERA_LOCATION = {'x': 7, 'y': 1, 'z': 0.125 + 0.315, 'yaw': 135 - 90, 'pitch': - 13.6, 'roll': 2, 'time': None}
+ZONE_3_INITIAL_CAMERA_LOCATION = {'x': 1, 'y': 1, 'z': 0.125 + 0.315, 'yaw': 45 - 90, 'pitch': - 13.6, 'roll': 2, 'time': None}
 
 ROBOT_TO_CAMERA_VECTOR = {'alpha': 0, 'beta': 0, 'gamma': 0.315} #alpha forwards, beta left to right, gamma up
 ROBOT_TO_DEFAULT_CAMERA_YAW = - 90
@@ -153,7 +153,7 @@ RIGHT_GRAB_OFFSET = 0
 
 #steady_thread
 
-MAX_CAMERA_ANGLE = 180 #deg
+MAX_CAMERA_ANGLE = 192 #deg
 MIN_CAMERA_ANGLE = 0 #deg
 CAMERA_TURN_RATE = 180 #output/sec
 CAMERA_STABILISATION_TIME = 0.1 #sec
