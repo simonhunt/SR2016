98259955aeb135da9839541b19274d3b9172360f
psc_aw1 <psc_aw1@srobo.org>
2016-04-26 19:08:28 +0000

    Commit message

--- noise.py
+++ noise.py
@@ -1,17 +1,26 @@
+READY_NOISE_DEBUG = True
+GOOD_NOISE_DEBUG = True
+BAD_NOISE_DEBUG = True
+ACTION_NOISE_DEBUG = True
+ACTIVITY_NOISE_DEBUG = True
+CAMERA_NOISE_DEBUG = True
+SERVO_NOISE_DEBUG = True
+TARGET_NOISE_DEBUG = True
+
 READY_NOTE = 'c'
-READY_DURATION = 500 #milliseconds
+READY_DURATION = 200 #milliseconds
 
 GOOD_NOTE = 'g'
-GOOD_DURATION = 1000 #milliseconds
+GOOD_DURATION = 300 #milliseconds
 
 BAD_NOTE = 'a'
-BAD_DURATION = 1000 #milliseconds
+BAD_DURATION = 300 #milliseconds
 
 ACTION_NOTE = 'b'
-ACTION_DURATION = 500 #milliseconds
+ACTION_DURATION = 200 #milliseconds
 
 ACTIVITY_NOTE = 'b'
-ACTIVITY_DURATION = 500 #milliseconds
+ACTIVITY_DURATION = 100 #milliseconds
 
 CAMERA_NOTE = 'd'
 CAMERA_DURATION = 100 #milliseconds
@@ -20,29 +29,44 @@ SERVO_NOTE = 'e'
 SERVO_DURATION = 100 #milliseconds
 
 TARGET_NOTE = 'f'
-TARGET_DURATION = 500 #milliseconds
+TARGET_DURATION = 100 #milliseconds
 
 def signalReady(power):
-    power.beep(READY_DURATION, note = READY_NOTE)
+    if (READY_NOISE_DEBUG == True):
+        power.beep(READY_DURATION, note = READY_NOTE)
     
 def signalGood(power):
-    power.beep(GOOD_DURATION, note = GOOD_NOTE)
+    
+    if (GOOD_NOISE_DEBUG == True):
+        power.beep(GOOD_DURATION, note = GOOD_NOTE)
 
 def signalBad(power):
-    power.beep(BAD_DURATION, note = BAD_NOTE)
+    
+    if (BAD_NOISE_DEBUG == True):
+        power.beep(BAD_DURATION, note = BAD_NOTE)
     
 def signalAction(power):
-    power.beep(ACTION_DURATION, note = ACTION_NOTE)
+    
+    if (ACTION_NOISE_DEBUG == True):
+        power.beep(ACTION_DURATION, note = ACTION_NOTE)
     
 def signalActivity(power):
-    power.beep(ACTIVITY_DURATION, note = ACTIVITY_NOTE)
+    
+    if (ACTIVITY_NOISE_DEBUG == True):
+        power.beep(ACTIVITY_DURATION, note = ACTIVITY_NOTE)
     
 def signalCamera(power):
-    power.beep(CAMERA_DURATION, note = CAMERA_NOTE)
+    
+    if (CAMERA_NOISE_DEBUG == True):
+        power.beep(CAMERA_DURATION, note = CAMERA_NOTE)
 
 def signalServo(power):
-    power.beep(SERVO_DURATION, note = SERVO_NOTE)
+    
+    if (SERVO_NOISE_DEBUG == True):
+        power.beep(SERVO_DURATION, note = SERVO_NOTE)
 
 def signalTarget(power):
-    power.beep(TARGET_DURATION, note = TARGET_NOTE)
+    
+    if (TARGET_NOISE_DEBUG == True):
+        power.beep(TARGET_DURATION, note = TARGET_NOTE)
     
