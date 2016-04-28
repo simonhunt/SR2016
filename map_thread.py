b7d58f577fec81d0cb64adde474213572e2b3ad5
psc_aw1 <psc_aw1@srobo.org>
2016-04-28 21:16:50 +0000

    Commit message

--- map_thread.py
+++ map_thread.py
@@ -14,10 +14,10 @@ MAX_CUBE_AGE = 5 #seconds
 MAX_BLIND_TIME = 1000 #seconds
 MAX_ARENA_MARKER_DISTANCE = 2 #meters
 
-MAX_D_X = 1 #meters
-MAX_D_Y = 1 #meters
-MAX_D_Z = 1 #meters
-MAX_D_PITCH = 45 #degrees
+MAX_D_X = 0.25 #meters
+MAX_D_Y = 0.25 #meters
+MAX_D_Z = 0.25 #meters
+MAX_D_PITCH = 30 #degrees
 MAX_D_YAW = 20 #degrees
 MAX_D_ROLL = 20 #degrees
 
