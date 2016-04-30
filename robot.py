4d8090bf2877dc790e17b7a6a7619aa02026cec6
psc_aw1 <psc_aw1@srobo.org>
2016-04-30 09:56:04 +0000

    Commit message

--- robot.py
+++ robot.py
@@ -344,59 +344,57 @@ def sonarTest():
         ServoThread.setSequence(positions.PHASE_0_ZERO)
         time.sleep (5)
         
+
+    
 time.sleep(5)
 
-TargetThread.setTarget(THREEFIVE)
-    
-# time.sleep(5)
+MotionThread.setAction(TURN_TO, - 45)
 
-# MotionThread.setAction(TURN_TO, - 45)
+time.sleep(10)
 
-# time.sleep(10)
+noise.signalGood(R.power)
 
-# noise.signalGood(R.power)
+getCubeDemo()
 
-# getCubeDemo()
+time.sleep(5)
 
-# time.sleep(5)
+MotionThread.setAction(TURN_TO, - 45)
 
-# MotionThread.setAction(TURN_TO, - 45)
+time.sleep(10)
 
-# time.sleep(10)
+noise.signalGood(R.power)
 
-# noise.signalGood(R.power)
+getCubeDemo()
 
-# getCubeDemo()
+time.sleep(5)
 
-# time.sleep(5)
+MotionThread.setAction(TURN_TO, - 45)
 
-# MotionThread.setAction(TURN_TO, - 45)
+time.sleep(10)
 
-# time.sleep(10)
+noise.signalGood(R.power)
 
-# noise.signalGood(R.power)
+getCubeDemo()
 
-# getCubeDemo()
+time.sleep(5)
 
-# time.sleep(5)
+MotionThread.setAction(TURN_TO, - 45)
 
-# MotionThread.setAction(TURN_TO, - 45)
+time.sleep(10)
 
-# time.sleep(10)
+noise.signalGood(R.power)
 
-# noise.signalGood(R.power)
+getCubeDemo()
 
-# getCubeDemo()
+time.sleep(5)
 
-# time.sleep(5)
+MotionThread.setAction(TURN_TO, - 45)
 
-# MotionThread.setAction(TURN_TO, - 45)
+time.sleep(10)
 
-# time.sleep(10)
+noise.signalGood(R.power)
 
-# noise.signalGood(R.power)
-
-# getCubeDemo()
+getCubeDemo()
 
 print "Main thread exited"
 
