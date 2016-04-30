a5fe9797adf6d3f3cf153ee9e952e86b0d01fc97
psc_aw1 <psc_aw1@srobo.org>
2016-04-30 22:03:38 +0000

    Commit message

--- first_cubes.py
+++ first_cubes.py
@@ -1,21 +1,21 @@
 from method import CUBEMAX_D_THETA
 
-ZONE_0_FIRST_CUBE_LOCATION = {'x': 2.5, 'y': 5.5, 'z': 0.125, 'yaw' = 0, 'pitch': 0, 'roll': 0, 'team_scoring': 0 'max_d_theta': CUBE_MAX_D_THEATA}
-ZONE_0_FIRST_APPROACH_LOCATION = {'x': 2.5, 'y', 7.5, 'z': 0.125, 'yaw' = 0, 'pitch': 0, 'roll': 0, 'degrees': 0}
+ZONE_0_FIRST_CUBE_LOCATION = {'x': 2.5, 'y': 5.5, 'z': 0.125, 'yaw': 0, 'pitch': 0, 'roll': 0, 'team_scoring': 0 'max_d_theta': CUBE_MAX_D_THEATA}
+ZONE_0_FIRST_APPROACH_LOCATION = {'x': 2.5, 'y', 7.5, 'z': 0.125, 'yaw': 0, 'pitch': 0, 'roll': 0, 'degrees': 0}
 ZONE_0_FIRST_CUBE_NET = "*"
 ZONE_0_STARTING_CUBE_APPROACH = {'cube_location': ZONE_0_FIRST_CUBE_LOCATION, 'approach_location': ZONE_0_FIRST_APPROACH_LOCATION, 'net': ZONE_0_FIRST_CUBE_NET}
 
-ZONE_1_FIRST_CUBE_LOCATION = {'x': 5.5, 'y': 5.5, 'z': 0.125, 'yaw' = 0, 'pitch': 0, 'roll': 0, 'team_scoring': 1 'max_d_theta': CUBE_MAX_D_THEATA}
-ZONE_1_FIRST_APPROACH_LOCATION = {'x': 6.5, 'y', 5.5, 'z': 0.125, 'yaw' = 0, 'pitch': 0, 'roll': 0, 'degrees': 0}
+ZONE_1_FIRST_CUBE_LOCATION = {'x': 5.5, 'y': 5.5, 'z': 0.125, 'yaw': 0, 'pitch': 0, 'roll': 0, 'team_scoring': 1 'max_d_theta': CUBE_MAX_D_THEATA}
+ZONE_1_FIRST_APPROACH_LOCATION = {'x': 6.5, 'y', 5.5, 'z': 0.125, 'yaw': 0, 'pitch': 0, 'roll': 0, 'degrees': 0}
 ZONE_1_FIRST_CUBE_NET = "*"
 ZONE_1_STARTING_CUBE_APPROACH = {'cube_location': ZONE_1_FIRST_CUBE_LOCATION, 'approach_location': ZONE_1_FIRST_APPROACH_LOCATION, 'net': ZONE_1_FIRST_CUBE_NET}
 
-ZONE_2_FIRST_CUBE_LOCATION = {'x': 5.5, 'y': 2.5, 'z': 0.125, 'yaw' = 0, 'pitch': 0, 'roll': 0, 'team_scoring': 2 'max_d_theta': CUBE_MAX_D_THEATA}
-ZONE_2_FIRST_APPROACH_LOCATION = {'x': 5.5, 'y', 1.5, 'z': 0.125, 'yaw' = 0, 'pitch': 0, 'roll': 0, 'degrees': 0}
+ZONE_2_FIRST_CUBE_LOCATION = {'x': 5.5, 'y': 2.5, 'z': 0.125, 'yaw': 0, 'pitch': 0, 'roll': 0, 'team_scoring': 2 'max_d_theta': CUBE_MAX_D_THEATA}
+ZONE_2_FIRST_APPROACH_LOCATION = {'x': 5.5, 'y', 1.5, 'z': 0.125, 'yaw': 0, 'pitch': 0, 'roll': 0, 'degrees': 0}
 ZONE_2_FIRST_CUBE_NET = "*"
 ZONE_2_STARTING_CUBE_APPROACH = {'cube_location': ZONE_2_FIRST_CUBE_LOCATION, 'approach_location': ZONE_2_FIRST_APPROACH_LOCATION, 'net': ZONE_2_FIRST_CUBE_NET}
 
-ZONE_3_FIRST_CUBE_LOCATION = {'x': 2.5, 'y': 2.5, 'z': 0.125, 'yaw' = 0, 'pitch': 0, 'roll': 0, 'team_scoring': 3 'max_d_theta': CUBE_MAX_D_THEATA}
-ZONE_3_FIRST_APPROACH_LOCATION = {'x': 1.5, 'y', 2.5, 'z': 0.125, 'yaw' = 0, 'pitch': 0, 'roll': 0, 'degrees': 0}
+ZONE_3_FIRST_CUBE_LOCATION = {'x': 2.5, 'y': 2.5, 'z': 0.125, 'yaw': 0, 'pitch': 0, 'roll': 0, 'team_scoring': 3 'max_d_theta': CUBE_MAX_D_THEATA}
+ZONE_3_FIRST_APPROACH_LOCATION = {'x': 1.5, 'y', 2.5, 'z': 0.125, 'yaw': 0, 'pitch': 0, 'roll': 0, 'degrees': 0}
 ZONE_3_FIRST_CUBE_NET = "*"
 ZONE_3_STARTING_CUBE_APPROACH = {'cube_location': ZONE_3_FIRST_CUBE_LOCATION, 'approach_location': ZONE_3_FIRST_APPROACH_LOCATION, 'net': ZONE_3_FIRST_CUBE_NET}
