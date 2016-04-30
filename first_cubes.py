69fc6544e1113456fa2514c527854af0695996d4
psc_aw1 <psc_aw1@srobo.org>
2016-04-30 21:48:54 +0000

    Commit message

--- first_cubes.py
+++ first_cubes.py
@@ -1,5 +1,11 @@
 from method import CUBEMAX_D_THETA
 
-ZONE_0_FIRST_CUBE_LOCATION = {'x': 2.5, 'y', 6.5, 'z': 0.125, 'yaw' = 0, 'pitch': 0, 'roll': 0, 'max_d_theta': CUBE_MAX_D_THEATA}
-ZONE_0_FIRST_APPROACH_LOCATION = {'x': 2.5, 'y', 7.5 'max_d_theta' = CUBE_MAX_D_THEATA}
-ZONE_0_STARTING_CUBE_APPROACH = {'cube_location': 
+ZONE_0_FIRST_CUBE_LOCATION = {'x': 2.5, 'y': 6.5, 'z': 0.125, 'yaw' = 0, 'pitch': 0, 'roll': 0, 'team_scoring': 0 'max_d_theta': CUBE_MAX_D_THEATA}
+ZONE_0_FIRST_APPROACH_LOCATION = {'x': 2.5, 'y', 7.5, 'z': 0.125, 'yaw' = 0, 'pitch': 0, 'roll': 0, 'degrees': 0}
+ZONE_0_FIRST_CUBE_NET = "*"
+ZONE_0_STARTING_CUBE_APPROACH = {'cube_location': ZONE_0_FIRST_CUBE_LOCATION, 'approach_location': ZONE_0_FIRST_APPROACH_LOCATION, 'net': ZONE_0_FIRST_CUBE_NET}
+
+ZONE_0_FIRST_CUBE_LOCATION = {'x': 2.5, 'y': 6.5, 'z': 0.125, 'yaw' = 0, 'pitch': 0, 'roll': 0, 'max_d_theta': CUBE_MAX_D_THEATA}
+ZONE_0_FIRST_APPROACH_LOCATION = {'x': 2.5, 'y', 7.5, 'z': 0.125, 'yaw' = 0, 'pitch': 0, 'roll': 0, 'degrees': 0}
+ZONE_0_FIRST_CUBE_NET = "*"
+ZONE_0_STARTING_CUBE_APPROACH = {'cube_location': ZONE_0_FIRST_CUBE_LOCATION, 'approach_location': ZONE_0_FIRST_APPROACH_LOCATION, 'net': ZONE_0_FIRST_CUBE_NET}
