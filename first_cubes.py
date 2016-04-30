c98438b0c12d3b770a0fd3319aa29dce3310795e
psc_aw1 <psc_aw1@srobo.org>
2016-04-30 21:24:26 +0000

    Commit message

---
+++ first_cubes.py
@@ -0,0 +1,5 @@
+from method import CUBEMAX_D_THETA
+
+ZONE_0_FIRST_CUBE_LOCATION = {'x': 2.5, 'y', 6.5, 'z': 0.125, 'yaw' = 0, 'pitch': 0, 'roll': 0, 'max_d_theta': CUBE_MAX_D_THEATA}
+ZONE_0_FIRST_APPROACH_LOCATION = {'x': 2.5, 'y', 7.5 'max_d_theta' = CUBE_MAX_D_THEATA}
+ZONE_0_STARTING_CUBE_APPROACH = {'cube_location': 
